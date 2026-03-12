08a8f90e4ca95114e17d92ac546b4051e014c123
24ya <saya8692@gmail.com>
2026-03-12 20:30:19 +0900

    Complete: BJ_10171_고양이

---
+++ algogo/src/BJ_10171_고양이_이사야.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class BJ_10171_고양이_이사야 {
+    public static void main(String[] args) throws IOException {
+        String cat ="\\    /\\\n" + //
+                        " )  ( ')\n" + //
+                        "(  /  )\n" + //
+                        " \\(__)|";
+        System.out.println(cat);
+    }
+}
